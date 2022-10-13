55efd1e241fa6212b7094e87c009c6ef92b5888a
phungo-hub <ngokieuphu@gmail.com>
2022-10-13 15:14:58 +0700

    add new files

---
+++ src/PointTest.java
@@ -0,0 +1,9 @@
+public class PointTest {
+    public static void main(String[] args) {
+        Point2D point2d = new Point2D(2,3);
+        Point3D point3d = new Point3D(2,3, 5);
+
+        System.out.println(point2d);
+        System.out.println(point3d);
+    }
+}
